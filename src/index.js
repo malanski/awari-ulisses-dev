ba37010a7a80ef808b4aa3d37a7ddc799ce1d461
malanski <malanskiwork@gmail.com>
2022-04-09 05:34:43 -0300

    remove provider and Store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store'
+// import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider> */}
       <App />
 
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Atividade5/Atividade5.js
+++ src/pages/Atividade5/Atividade5.js
@@ -1,17 +1,41 @@
 import '../../assets/scss/Atividade5.scss';
-import atividade5 from '../../assets/images/atividade5.pdf';
 import atividade5print from '../../assets/images/ulisses_malanski_front-end_07-04-22.pdf';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCertificate } from '@fortawesome/free-solid-svg-icons';
-
+import atividade5print2 from '../../assets/images/ulisses_malanski_front-end_4-8-22.pdf';
 
 export const Atividade5 = (props) => {
     return (
 
         <div className='atividade5'>
             <h1>Curriculum</h1>
+
+            <h2>
+                
+                HTML version: <a href="https://malanski.github.io/curriculum-eng/">
+                    Link Curriculo HTML
+                </a>
+            </h2>
+
+            
+            <embed 
+                    src='https://malanski.github.io/curriculum-eng/'
+                    
+                    width='90%'
+                    height='1000px' 
+                />
+
+            <br/>
+            
             <h2>Printable PDF version:</h2>
+            
+            <embed 
+                src={atividade5print2}
+                
+                width='90%'
+                height='1000px' 
+            />
+            <br/>    
+
+            <h2>Portuguese PDF version:</h2>
 
             <embed 
                 src={atividade5print}
@@ -19,25 +43,6 @@ export const Atividade5 = (props) => {
                 width='90%'
                 height='1000px' 
             />
-
-            <br/>    
-
-            <h2>
-                
-                HTML version: <a href="https://malanski.github.io/curriculo/">
-                    Link Curriculo HTML
-                </a>
-            </h2>
-
-            
-            <embed 
-                    src='https://malanski.github.io/curriculo/'
-                    
-                    width='90%'
-                    height='1000px' 
-                />
-
-            <br/>
 
         </div>
     )
